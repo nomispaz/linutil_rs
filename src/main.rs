10c6a995e1577e83657397e438a415f153520992
nomispaz <l0rdp4z1f1st@gmail.com>
2024-09-16 15:10:18 +0200

    finished first version of git clone and push

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,12 @@ pub enum CurrentScreen {
     List
 }
 
-pub enum Mode {
-    clone,
-    push
+pub enum CurrentMode {
+    CloneMode,
+    None,
+    PushMode,
+    CloneExecute,
+    PushExecute
 }
 
 // contains the state of the app:
@@ -32,7 +35,7 @@ pub struct App {
     pub current_screen: CurrentScreen,
     pub list_screen: ListWidget,
     pub restart: bool,
-    pub mode: String
+    pub current_mode: CurrentMode
 }
 
 impl App {
@@ -41,33 +44,57 @@ impl App {
             current_screen: CurrentScreen::Main,
             list_screen: ListWidget::new(vec!["Clone repos".to_string(), "Push repos".to_string()]),
             restart: false,
-            mode: "".to_string()
+            current_mode: CurrentMode::None
         }
     }
 }
 
-fn handle_list_selection(app: &mut App) {
-    if app.mode == "Clone repos" {
-	let commands: Vec<&str> = vec!["curl https://api.github.com/users/nomispaz/repos | grep full_name | cut -d':' -f 2 | cut -d'\"' -f 2 | cut -d'/' -f 2"];
-        let output = run_commands(commands).expect("Test");
-        let vec_out =String::from_utf8_lossy(&output.stdout).into_owned();
-        let lines: Vec<String> = vec_out.split('\n').map(|s| s.into()).collect();
-        app.list_screen.set_items(lines);
-    }
-    else {
-        println!("Test");
+fn handle_mode_selection(app: &mut App) -> Result<bool, Error> {
+    let mut commands = Vec::new();
+
+    match app.current_mode {
+        CurrentMode::CloneMode => {
+            commands.push("curl https://api.github.com/users/nomispaz/repos | grep full_name | cut -d':' -f 2 | cut -d'\"' -f 2 | cut -d'/' -f 2");
+            let output = run_commands_stdout(&commands).expect("TODO");
+            let vec_out =String::from_utf8_lossy(&output.stdout).into_owned();
+            let lines: Vec<String> = vec_out.split('\n').map(|s| s.into()).collect();
+            app.list_screen.set_items(lines);
+            return Ok(false)
+        }
+        CurrentMode::PushMode => {
+            commands.push("ls /home/simonheise/git_repos/");
+            let output = run_commands_stdout(&commands).expect("TODO");
+            let vec_out =String::from_utf8_lossy(&output.stdout).into_owned();
+            let lines: Vec<String> = vec_out.split('\n').map(|s| s.into()).collect();
+            app.list_screen.set_items(lines);
+            return Ok(false)
+        }
+        CurrentMode::CloneExecute | CurrentMode::PushExecute => {
+            app.restart = true;
+            return Ok(true)
+        }
+        // handle all other possibilities
+        _ => return Ok(false)
     }
 }
 
 fn run(mut terminal: DefaultTerminal, app: &mut App) -> io::Result<()> {
     //! takes a terminal and runs the app.
 
-    let mut greeting_text = "Hello Ratatui! (press 'q' to quit)".to_string();
-
     loop {
 
         // draw the main frame
         terminal.draw(|frame| {        
+
+            // generate text for start window
+            let mut main_text = "Keyboard shortcuts:\n".to_string();
+            main_text.push_str("-h: return to this page\n");
+            main_text.push_str("-l: open list of possible operations\n");
+            main_text.push_str("-m: return to default list\n");
+            main_text.push_str("-arrow keys: select entry in list\n");
+            main_text.push_str("-Enter: Perform selected action\n");
+            main_text.push_str("\n");
+
 
             // The items managed by the application are transformed to something
             // that is understood by ratatui.
@@ -85,13 +112,13 @@ fn run(mut terminal: DefaultTerminal, app: &mut App) -> io::Result<()> {
                 .repeat_highlight_symbol(true)
                 .direction(ListDirection::TopToBottom);
    
-            let greeting = Paragraph::new(greeting_text.clone())
+            let main = Paragraph::new(main_text)
                 .white()
                 .on_blue();
 
             // draw the widget corresponding to the current_screen
             match app.current_screen {
-                CurrentScreen::Main => frame.render_widget(greeting, frame.area()),
+                CurrentScreen::Main => frame.render_widget(main, frame.area()),
                 CurrentScreen::List => frame.render_stateful_widget(list, frame.area(), &mut app.list_screen.state)
             }
         })?;
@@ -103,10 +130,13 @@ fn run(mut terminal: DefaultTerminal, app: &mut App) -> io::Result<()> {
                     KeyCode::Char('q') => {
                         app.restart = false;
                         return Ok(())
-                    }                    KeyCode::Char('l') => {
-                        app.current_screen = CurrentScreen::List;
+                    }                    
+                    KeyCode::Char('l') => app.current_screen = CurrentScreen::List,
+                    KeyCode::Char('h') => app.current_screen = CurrentScreen::Main,
+                    KeyCode::Char('m') => {
+                        app.list_screen.set_items(vec!["Clone repos".to_string(), "Push repos".to_string()]);
+                        app.current_mode = CurrentMode::None;
                     }
-                    KeyCode::Char('g') => app.current_screen = CurrentScreen::Main,
                     KeyCode::Up | KeyCode::Left => app.list_screen.previous(),
                     KeyCode::Down | KeyCode::Right => app.list_screen.next(),
                     KeyCode::Backspace => {
@@ -114,15 +144,35 @@ fn run(mut terminal: DefaultTerminal, app: &mut App) -> io::Result<()> {
                         return Ok(())
                     }
                     KeyCode::Enter => {
-                        app.mode = app.list_screen.selected_item().unwrap().clone();
-                        handle_list_selection(app);
+                        match app.current_mode {
+                            CurrentMode::None => {
+                                let current_mode = app.list_screen.selected_item().unwrap();
+                                if current_mode == "Clone repos" {
+                                    app.current_mode = CurrentMode::CloneMode
+                                }
+                                if current_mode == "Push repos" {
+                                    app.current_mode = CurrentMode::PushMode
+                                }
+                            }
+                            CurrentMode::CloneMode => {
+                                app.current_mode = CurrentMode::CloneExecute
+                            }
+                            CurrentMode::PushMode => {
+                                app.current_mode = CurrentMode::PushExecute
+                            }
+                            _ => ()
+                        }
+                        let close_tui = handle_mode_selection(app).unwrap();
+                        if close_tui {
+                            return Ok(())
+                        }
                     }                    
                     KeyCode::Tab => {
                         if let Some(index) = app.list_screen.state.selected() {
                             if index < app.list_screen.items.len() {
                                 // read output of command, convert it to vec<str> and update the
                                 // list
-                                let output = run_commands(vec!["ls -l"]).expect("Test");
+                                let output = run_commands_stdout(&vec!["ls -l"]).expect("TODO");
                                 let vec_out =String::from_utf8_lossy(&output.stdout).into_owned();
                                 let lines: Vec<String> = vec_out.split('\n').map(|s| s.into()).collect();
                                 app.list_screen.set_items(lines);
@@ -138,7 +188,19 @@ fn run(mut terminal: DefaultTerminal, app: &mut App) -> io::Result<()> {
     }
 }
 
-fn run_commands(commands: Vec<&str>) -> Result<Output, std::io::Error> {
+fn run_commands(commands: Vec<&str>) {
+    //! takes vector of bash commands and executes the commands
+    let joined_command = commands.join("; ");
+    let mut cmd = Command::new("bash");
+    cmd.arg("-c")
+        .arg(joined_command)
+        .spawn()
+        .expect("Error: Failed to run editor")
+        .wait()
+        .expect("Error: Editor returned a non-zero status");
+}
+
+fn run_commands_stdout(commands: &Vec<&str>) -> Result<Output, std::io::Error> {
     //! takes vector of bash commands and executes the commands
     let joined_command = commands.join("; ");
     let mut cmd = Command::new("bash");
@@ -185,23 +247,40 @@ fn main() -> io::Result<()> {
 
     while app.restart {
 
-        println!("Enter something (type 'exit' to quit):");
+        let mut commands: Vec<&str> = Vec::new();
+        let selected_item: String = app.list_screen.selected_item().unwrap().to_string();
+        let mut command_prep: String = String::new();
+        match app.current_mode {
+            CurrentMode::CloneExecute => {
+                command_prep.push_str(&format!("git clone https://github.com/nomispaz/{selected_item} /home/simonheise/git_repos/test/{selected_item}"));
+            }
+            CurrentMode::PushExecute => {
+                let mut input = String::new();
+                println!("\nEnter commit message:  ");
+                io::stdin().read_line(&mut input)?;
+
+                command_prep.push_str(&format!("pushd /home/simonheise/git_repos/{selected_item};"));
+                command_prep.push_str(&format!("git add .;"));
+                command_prep.push_str(&format!("git commit -m \"{}\";", input));
+                command_prep.push_str(&format!("git push;"));
+                command_prep.push_str(&format!("popd"));
+            }
+            _ => ()
+        }
+        commands.push(&command_prep);
+        run_commands(commands);
+       
+        println!("\nHit ENTER to return to continue or type 'exit' to quit:");
 
         let stdin = io::stdin();
-        loop {
-            let mut input = String::new();
+        let mut input = String::new();
 
-            print!("> ");
-
-            stdin.read_line(&mut input)?;
-
-            if input.trim() == "exit" {
-                break;
-            }
-
-            println!("You said: {}", input.trim());
+        print!(">> ");
+        stdin.read_line(&mut input)?;
+        if input.trim() == "exit" {
+            break;
         }
-
+        
         terminal = ratatui::init();
         // clear terminal
         terminal.clear()?;
